21db1ac51e270d9a917f60fd72f7aadea2780483
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-26 21:26:38 +0800

    Pizza order exercise

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-year = int(input("Which year do you want to check? "))
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f"{year} is a leap year.")
-        else:
-            print(f"{year} is not a leap year.")
-    else:
-        print(f"{year} is a leap year.")
-else:
-    print(f"{year} is not a leap year.")
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size of Pizza do you want? S, M, or L? ")
+add_pepperoni = input("Do you want to add pepperoni? Y or N. ")
+extra_cheese = input("Do you want to add extra cheese? Y or N. ")
+
+bill = 0
+if size == "S":
+    bill = 15
+    if add_pepperoni == "Y":
+        bill +=2
+    if extra_cheese == "Y":
+        bill +=1
+elif size == "M":
+    bill = 20
+    if add_pepperoni == "Y":
+        bill +=3
+    if extra_cheese == "Y":
+        bill +=1
+elif size == "L":
+    bill = 25
+    if add_pepperoni == "Y":
+        bill +=3
+    if extra_cheese == "Y":
+        bill +=1
+
+print(f"Your final bill is ${bill}.")
