9189fda6c63bdeeaee7e73c78eb5cb139ba081be
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-26 20:25:42 +0800

    elif

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-number = int(input("Which number do you want to check? "))
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
 
-if number % 2 == 0:
-    print("This is an even number.")
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("How old are you? "))
+    if age < 12:
+        print("Pay $5.")
+    elif age <= 18:
+        print("Pay $7.")
+    else:
+        print("Pay $12.")
 else:
-    print("This is an odd number.")
+    print("Sorry!")
