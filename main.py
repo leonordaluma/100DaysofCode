cfbc6ff3ab543dc9c79d9b7e95db6449148a813b
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-26 20:44:34 +0800

    bmi 2.0 exercise

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
-
-
-if height > 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("How old are you? "))
-    if age < 12:
-        print("Pay $5.")
-    elif age <= 18:
-        print("Pay $7.")
-    else:
-        print("Pay $12.")
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+calculation = weight / height ** 2
+bmi = int(round(calculation))
+print(f"{weight} / {height} * {height} = {calculation}")
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
 else:
-    print("Sorry!")
+    print(f"Your BMI is {bmi}, you are clinically obese.")
