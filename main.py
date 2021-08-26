cdec6914a26f73ca63d417daf2fd5658c134f837
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-26 12:56:10 +0800

    simplified life in weeks

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 age = input("What is your current age? ")
 age_in_int = int(age)
-past_days = age_in_int * 365
-past_weeks = age_in_int * 52
-past_months = age_in_int * 12
-remaining_days = (365 * 90) - past_days
-remaining_weeks = (52 * 90) - past_weeks
-remaining_months = (12 * 90) - past_months
+remaining_years = 90 - age_in_int
+remaining_days = remaining_years * 365
+remaining_weeks = remaining_years * 52
+remaining_months = remaining_years * 12
 print(f"You have {remaining_days}, {remaining_weeks} weeks and {remaining_months} left.")
