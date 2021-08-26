235cba50e44f066cca691d73b17f1a37179337f4
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-26 12:55:38 +0800

    life in weeks

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-height = float(input("Enter your height in m: "))
-weight = float(input("Enter your weight in kg: "))
-calculation = weight / height ** 2
-bmi = int(calculation)
-print(f"{weight} / {height} * {height} = {calculation}")
-print(bmi)
+age = input("What is your current age? ")
+age_in_int = int(age)
+past_days = age_in_int * 365
+past_weeks = age_in_int * 52
+past_months = age_in_int * 12
+remaining_days = (365 * 90) - past_days
+remaining_weeks = (52 * 90) - past_weeks
+remaining_months = (12 * 90) - past_months
+print(f"You have {remaining_days}, {remaining_weeks} weeks and {remaining_months} left.")
