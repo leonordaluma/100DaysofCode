8334824dd45ce85c8bbaacb456c756750fb71ebe
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-26 12:54:21 +0800

    bmi calculation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-two_digit_number = input("Type a two digit number: ")
-num1 = two_digit_number[0]
-num2 = two_digit_number[1]
-print(int(num1) + int(num2))
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in kg: ")
+calculation = float(weight) / float(height) ** 2
+bmi = int(calculation)
+print(weight + " / " + height + " * " + height + " = " + str(calculation))
+print(bmi)
