1dff665436e1624b8daa79639661c9efe7468ad3
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-26 21:32:43 +0800

    simplified pizza order exercise

--- main.py
+++ main.py
@@ -5,22 +5,19 @@ extra_cheese = input("Do you want to add extra cheese? Y or N. ")
 
 bill = 0
 if size == "S":
-    bill = 15
-    if add_pepperoni == "Y":
-        bill +=2
-    if extra_cheese == "Y":
-        bill +=1
+    bill += 15
 elif size == "M":
-    bill = 20
-    if add_pepperoni == "Y":
-        bill +=3
-    if extra_cheese == "Y":
-        bill +=1
-elif size == "L":
-    bill = 25
-    if add_pepperoni == "Y":
-        bill +=3
-    if extra_cheese == "Y":
-        bill +=1
+    bill += 20
+else:
+    bill += 25
 
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+        
 print(f"Your final bill is ${bill}.")
