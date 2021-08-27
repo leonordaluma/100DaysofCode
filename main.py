139de1ef070c06f9f0b3a79e85f4be067c057090
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-27 22:50:47 +0800

    simplified treasure map exercise

--- main.py
+++ main.py
@@ -6,23 +6,7 @@ print(f"{row1}\n{row2}\n{row3}\n")
 position = input("Where do you want to put the treasure? ")
 column = int(position[0])
 row = int(position[1])
-if column == 1 and row == 1:
-    row1[0] = "X"
-elif column == 1 and row == 2:
-    row2[0] = "X"
-elif column == 1 and row == 3:
-    row3[0] = "X"
-elif column == 2 and row == 1:
-    row1[1] = "X"
-elif column == 2 and row == 2:
-    row2[1] = "X"
-elif column == 2 and row == 3:
-    row3[1] = "X"
-elif column == 3 and row == 1:
-    row1[2] = "X"
-elif column == 3 and row == 2:
-    row2[2] = "X"
-elif column == 3 and row == 3:
-    row3[2] = "X"
+
+map[column-1][row - 1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}\n")
