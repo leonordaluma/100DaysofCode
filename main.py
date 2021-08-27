26aff84601d2f00bb8b38158ac9d32660af17526
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-27 19:49:49 +0800

    banker roulette exercise

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 test_seed = int(input("Create a seed number: "))
 random.seed(test_seed)
-random_int = random.randint(0,1)
 
-if random_int == 0:
-    print("Tails")
-else:
-    print("Heads")
+nameAsCSV = input("Give me everybody's names, separated by a comma. ")
+names = nameAsCSV.split(", ")
+indices = len(names)
+random_index = random.randint(0, indexes - 1)
+print(f"{names[random_index]} is going to buy the meal today!")
+# print(indexes)
