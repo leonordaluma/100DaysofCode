a30a86d111f5bef8bcb7d3b2a7f17da1bd2f5211
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-28 17:42:29 +0800

    5.3 adding even numbers

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-student_scores = input("Input a list of student scores ").split()
+total = 0
+for n in range(1,101):
+    if n % 2 == 0:
+        total += n    
+print(total)
 
-for n in range(0, len(student_scores)):
-    student_scores[n] = int(student_scores[n])
-print(student_scores)
-
-max_score = 0
-for n in student_scores:
-    current_number = n
-    if current_number > max_score:
-        max_score = current_number
-    else:
-        temp = current_number
-
-print(f"The highest score is: {max_score}")
+# for n in range(2, 101, 2):
+#     total +=n
+# print(total)
