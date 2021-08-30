1c078aa26ea5805e98679145411e24d4e7c6f93b
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-30 17:55:31 +0800

    hangman challenge 2

--- hangman.py
+++ hangman.py
@@ -1,13 +1,17 @@
 import random
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+display = []
+for letter in chosen_word:
+    display += "_"
 print(chosen_word)
+print(display)
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for letter in range(0, len(chosen_word)):
+    if guess == chosen_word[letter]:
+        display[letter] = guess
+
+print(display)
 
 
