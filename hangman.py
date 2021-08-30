0e30bfb73ac6c4a91e3675fc8be90d37ac1b54ec
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-30 17:37:21 +0800

    simplified hangman challenge 1

--- hangman.py
+++ hangman.py
@@ -4,8 +4,8 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 guess = input("Guess a letter: ").lower()
 
-for letter in range(0, len(chosen_word)):
-    if guess == chosen_word[letter]:
+for letter in chosen_word:
+    if letter == guess:
         print("Right")
     else:
         print("Wrong")
