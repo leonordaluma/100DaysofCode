93d49ef58cff227366eb3bbbcba7b7e0bbe5fc5a
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-31 15:12:11 +0800

    hangman challenge 4

--- hangman.py
+++ hangman.py
@@ -1,4 +1,61 @@
 import random
+stages = ['''
+    +---+
+    |   |
+    0   |
+   /|\  |
+   / \  |
+        |
+========= 
+''', '''
+    +---+
+    |   |
+    0   |
+   /|\  |
+   /    |
+        |
+========= 
+''', '''
+    +---+
+    |   |
+    0   |
+   /|\  |
+        |
+        |
+========= 
+''', '''
+    +---+
+    |   |
+    0   |
+   /|   |
+        |
+        |
+========= 
+''', '''
+    +---+
+    |   |
+    0   |
+   /    |
+        |
+        |
+========= 
+''', '''
+    +---+
+    |   |
+    0   |
+        |
+        |
+        |
+========= 
+''', '''
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+========= 
+''']
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 display = []
@@ -6,6 +63,7 @@ for letter in chosen_word:
     display += "_"
 print(chosen_word)
 print(display)
+lives = 6
 end_of_game = False
 
 while not end_of_game:
@@ -14,6 +72,13 @@ while not end_of_game:
       if guess == chosen_word[letter]:
         display[letter] = guess
     print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
     if "_" not in display:
         end_of_game = True
