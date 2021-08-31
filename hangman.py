24542d55248c304f702079b03457f3fed990d3ae
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-08-31 17:57:08 +0800

    day 7

--- hangman.py
+++ hangman.py
@@ -1,81 +1,41 @@
 import random
-stages = ['''
-    +---+
-    |   |
-    0   |
-   /|\  |
-   / \  |
-        |
-========= 
-''', '''
-    +---+
-    |   |
-    0   |
-   /|\  |
-   /    |
-        |
-========= 
-''', '''
-    +---+
-    |   |
-    0   |
-   /|\  |
-        |
-        |
-========= 
-''', '''
-    +---+
-    |   |
-    0   |
-   /|   |
-        |
-        |
-========= 
-''', '''
-    +---+
-    |   |
-    0   |
-   /    |
-        |
-        |
-========= 
-''', '''
-    +---+
-    |   |
-    0   |
-        |
-        |
-        |
-========= 
-''', '''
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-========= 
-''']
-word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+import hangman_art
+import hangman_words
+
+print(hangman_art.logo[0])
+
+
+chosen_word = random.choice(hangman_words.word_list)
 display = []
 for letter in chosen_word:
     display += "_"
 print(chosen_word)
-print(display)
+
 lives = 6
 end_of_game = False
+occurance = ""
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    if guess == occurance:
+        print(f"{guess} is already guessed. \n")
+    # or 
+    # if guess in display:
+    #     print(f"{guess} is already guessed. \n")
+    
     for letter in range(0, len(chosen_word)):
       if guess == chosen_word[letter]:
         display[letter] = guess
-    print(display)
+        occurance = guess
+
+    print(f"{' '.join(display)}")
+
+
 
     if guess not in chosen_word:
         lives -= 1
-        print(stages[lives])
+        print(f"You guessed {guess}, that is not the word. You lose a life.")
+        print(hangman_art.stages[lives])
         if lives == 0:
             end_of_game = True
             print("You lose.")
@@ -83,3 +43,4 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
+
