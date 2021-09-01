5622f145702614d9806c2f3fdda702f93be7639c
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-02 06:03:05 +0800

    caesar cipher part 1 - encrypt

--- caesar cipher challenge/caesar_cipher.py
+++ caesar cipher challenge/caesar_cipher.py
@@ -1,15 +1,17 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def encrypt(text, shift):
+    new_text = ""
     cipher_text =""
     length = len(alphabet)
-    for letter in text:
-        for n in range(length):
-            if letter == alphabet[n]:
-                key = n+shift
-                print(alphabet[key])
-                print(key)
-                # cipher_text += alphabet[key]
+    for i in text:
+        if i!=" ":
+            new_text+=i
+    for letter in new_text:
+        position = alphabet.index(letter)
+        if alphabet[position] == letter:
+            key = position+shift
+            cipher_text += alphabet[key]
     print(f"The encoded text is {cipher_text}")
 
 
