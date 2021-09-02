6dc4d3f041f1a0b60d05fce0ed3043706b23ae05
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-02 23:13:50 +0800

    day 8

--- caesar cipher challenge/caesar_cipher.py
+++ caesar cipher challenge/caesar_cipher.py
@@ -1,24 +1,32 @@
+import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(text, shift, direction):
-    new_text = ""
     plain_text = ""
-    # concatanate the text to remove spaces
-    for i in text:
-        if i!=" ":
-            new_text+=i
-            
-    for letter in new_text:
-        position = alphabet.index(letter)
-        if alphabet[position] == letter:
-            if direction == "encode":
-                key = position + shift
-            elif direction == "decode":
-                key = position - shift
-        plain_text += alphabet[key]
+    for letter in text:
+        if letter not in alphabet:
+            plain_text += letter
+        else:
+            position = alphabet.index(letter)
+            if alphabet[position] == letter:
+                if direction == "encode":
+                    key = position + shift
+                elif direction == "decode":
+                    key = position - shift
+            plain_text += alphabet[key]
+        
+
     print(f"The {direction}d text is {plain_text}")        
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number: "))
-caesar(text = text, shift = shift, direction = direction)
+answer = "yes"
+
+while answer == "yes":
+    print(art.logo[0])
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number: "))
+    print(shift)
+    shift = shift % 25
+    print(shift)
+    caesar(text = text, shift = shift, direction = direction)
+    answer = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
