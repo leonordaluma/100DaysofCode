35bc146d54a68b6228b9af4bb45650c1e2a4ae50
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-03 18:04:51 +0800

    function with output

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-travel_log = [
-    {
-        "country" : "France",
-        "total_visits" : 12,
-        "cities_visited" : ["Paris", "Lille", "Dijon"],
-    },
-    {
-        "country" : "Germany",
-        "total_visits" : 2,
-        "cities_visited" : ["Berlin", "Hamburg", "Stuttgard"],
-    }
-]
+def format_name(f_name, l_name):
+    F_name = f_name.title()
+    L_name = l_name.title()
+    return f"{F_name} {L_name}"
 
-def add_new_country(country, visits, cities):
-    new_country = {}
-    new_country["country"] = country
-    new_country["total_visits"] = visits
-    new_country["cities_visited"] = cities
-    travel_log.append(new_country)
-    
 
-add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travel_log)
+print(format_name("leonor", "daluma"))
