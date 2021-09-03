eb418fd2bf569f4fe1156a644429406b650e5039
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-03 18:49:45 +0800

    10.1 days in month exercise

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-def format_name(f_name, l_name):
-    F_name = f_name.title()
-    L_name = l_name.title()
-    return f"{F_name} {L_name}"
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 
+def days_in_month(year, month):
+    month -=1
+    month_days =[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year):
+        month_days[1] = 29
+    return month_days[month]
 
-print(format_name("leonor", "daluma"))
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
