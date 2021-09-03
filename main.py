c8523ce5c2aca26b4753a5cd3e4ab3222a8f0299
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-03 09:06:49 +0800

    9.1 grading program exercise

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        print(i)
-        if number % i == 0:
-            is_prime = False
-    if is_prime:
-        print("It's a prime.")
-    else:
-        print("It's not a prime.")
+student_scores = {
+    "Harry" : 81,
+    "Ron" : 78,
+    "Hermione" : 99,
+    "Draco" : 74,
+    "Neville" : 62,
+}
 
-    
+student_grades = {}
 
-n = int(input("Check this number: "))
-prime_checker(number = n)
+for score in student_scores:
+    score_value = student_scores[score]
+    if score_value >= 91 and score_value <= 100:
+        student_grades[score] = "Outstanding"
+    elif score_value >= 81 and score_value <= 90:
+        student_grades[score] = "Exceeds Expectations"
+    elif score_value >= 71 and score_value <= 80:
+        student_grades[score] = "Acceptable"
+    elif score_value <= 70:
+        student_grades[score] = "Fail"
+
+print(student_grades)
