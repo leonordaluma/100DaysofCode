3340b3c0658aa33549b440abac29e3bbc44028dd
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-03 19:36:14 +0800

    return

--- calculator.py
+++ calculator.py
@@ -26,3 +26,9 @@ operation_symbol = input("Pick an operation from the line above: ")
 function = operations[operation_symbol]
 answer = function(num1, num2)
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("Enter the next number: "))
+function = operations[operation_symbol]
+answer2 = function(answer, num3)
+print(f"{answer} {operation_symbol} {num3} = {answer2}")
