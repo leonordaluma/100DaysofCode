af0018de9646e1c60e5f6fbf79585edcf7d32f32
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-08 19:08:48 +0800

    namespaces

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+def drink_potion():
+    print(player_health)
 
-def days_in_month(year, month):
-    month -=1
-    month_days =[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year):
-        month_days[1] = 29
-    return month_days[month]
-
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
-
+player_health = 10
+drink_potion()
