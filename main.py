82ed4639585cd5670371dcb71fd5f094f84531d6
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-09 17:33:40 +0800

    13.1 debugging odd or even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-year = int(input("What's your year of birth? "))
-if year > 1980 and year <= 1994:
-    print("You are a millenial.")
-elif year > 1994:
-    print("You are a Gen Z.")
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
