01fa3e5566cfac0faaecc60a07499db3c5d415cd
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-10 13:01:21 +0800

    higher lower - game files

---
+++ higher lower/game_data.py
@@ -0,0 +1,38 @@
+data = [
+    {
+        'name' : 'Instagram',
+        'follower_count': 346,
+        'description': 'Social Media Platform',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Christiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country' : 'Portugal'
+    },
+    {
+        'name' : 'Ariana Grande',
+        'follower_count': 183,
+        'description': 'Musician and Actress',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and Professional Wrestler',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Selena Gomez',
+        'follower_count': 174,
+        'description': 'Musician and Actress',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Kylie Jenner',
+        'follower_count': 172,
+        'description': 'Reality TV Personality and Businesswoman and Self-Made Billionaire',
+        'country' : 'United States'
+    },
+]
---
+++ higher lower/main.py
@@ -0,0 +1,9 @@
+from art import logo, vs
+from game_data import data
+
+print(logo)
+print("Compare A: ")
+print(vs)
+print("Against B: ")
+answer = input("Who has more followers? Type 'A' or 'B': ").upper()
+print(answer)
