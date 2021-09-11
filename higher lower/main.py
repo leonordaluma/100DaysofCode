1abc4551a75abdc9a0c658b4b756c56a8957e06b
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-11 18:32:30 +0800

    updated game files

--- higher lower/main.py
+++ higher lower/main.py
@@ -1,8 +1,19 @@
 from art import logo, vs
 from game_data import data
+from random import choice
 
+def name_generator():
+    name = choice(data)
+    return name
+    
 print(logo)
-print("Compare A: ")
+a = name_generator()
+name = a["name"]
+follower_count = a["follower_count"]
+description = a["description"]
+country = a["country"]
+print(f"Compare A: {name}, a(n) {description}, from {country}.")
+
 print(vs)
 print("Against B: ")
 answer = input("Who has more followers? Type 'A' or 'B': ").upper()
