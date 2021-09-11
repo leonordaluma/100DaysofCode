7e7781e05efbdc61bbeaab0c98b8b1c2238c61f1
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-11 18:49:23 +0800

    higher lower wip

--- higher lower/main.py
+++ higher lower/main.py
@@ -5,16 +5,36 @@ from random import choice
 def name_generator():
     name = choice(data)
     return name
-    
-print(logo)
-a = name_generator()
-name = a["name"]
-follower_count = a["follower_count"]
-description = a["description"]
-country = a["country"]
-print(f"Compare A: {name}, a(n) {description}, from {country}.")
 
-print(vs)
-print("Against B: ")
-answer = input("Who has more followers? Type 'A' or 'B': ").upper()
-print(answer)
+def compare_follower_count(a_followers,b_followers):
+    return max(a_followers, b_followers)
+
+def fetch_data(empty_dict):
+    empty_dict = name_generator()
+    
+
+
+a = {}
+b = {}
+
+print(logo)
+is_wrong = False
+while is_wrong:
+    a = name_generator()
+    name = a["name"]
+    follower_count = a["follower_count"]
+    description = a["description"]
+    country = a["country"]
+    print(f"Compare A: {name}, a(n) {description}, from {country}.")
+
+    print(vs)
+    b = name_generator()
+    name = b["name"]
+    follower_count = b["follower_count"]
+    description = b["description"]
+    country = b["country"]
+    print(f"Against B: {name}, a(n) {description}, from {country}. ")
+    answer = input("Who has more followers? Type 'A' or 'B': ").upper()
+    
+    print(answer)
+
