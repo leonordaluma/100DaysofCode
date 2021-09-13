7821a3857d771970ec02b5dd98ef344510cc7078
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-13 11:06:26 +0800

    higher lower - different version

--- higher lower/test.py
+++ higher lower/test.py
@@ -2,22 +2,43 @@ from art import logo, vs
 from game_data import data
 from random import choice
 
-def select_person():
-    person = choice(data)
-    return person
+def format_data(account):
+    account_name = account["name"]
+    account_desc = account["description"]
+    account_country = account["country"]
+    return(f"{account_name}, a {account_desc}, from {account_country}")
 
-def printable_format(person):
-    name = person["name"]
-    follower_count = person["follower_count"]
-    description = person["description"]
-    country = person["country"]
-    
-
-    
-
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
 print(logo)
-print(f"Compare A: {name}, a(n) {description}, from {country}.")
-a = select_person()
-b = select_person()
+score = 0
+game_should_continue = True
+account_b = choice(data)
 
+while game_should_continue:
+    account_a = account_b
+    account_b = choice(data)
+
+    while account_a == account_b:
+        account_b = choice(data)
+
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}")
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        game_should_continue = False
+        print(f"Sorry, you're wrong. Final score: {score}")
+        
