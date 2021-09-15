f749911e3a879b5c01982e1be61e1c892eeba3ce
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-15 17:22:38 +0800

    modifying object attributes and calling methods

--- OOP/main.py
+++ OOP/main.py
@@ -1,13 +1,21 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-rafael = Turtle()
-timmy.shape("turtle")
-rafael.shape("turtle")
-rafael.color('blue')
-timmy.color('red')
-timmy.forward(100)
+# timmy = Turtle()
+# rafael = Turtle()
+# timmy.shape("turtle")
+# rafael.shape("turtle")
+# rafael.color('blue')
+# timmy.color('red')
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type ", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
