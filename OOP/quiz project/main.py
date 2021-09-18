d357bf62155886da3b37e47686954b4371a44616
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-18 17:49:16 +0800

    quiz project - check if there are remaining questions

--- OOP/quiz project/main.py
+++ OOP/quiz project/main.py
@@ -8,4 +8,6 @@ for question in question_data:
     question_bank.append(questions)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
