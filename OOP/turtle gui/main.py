89b198bda5cef6bae090a158068537cb04dc545b
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-19 14:21:36 +0800

    turtle challenge 2- draw a dashed line

--- OOP/turtle gui/main.py
+++ OOP/turtle gui/main.py
@@ -1,20 +1,13 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
-tim.shape("turtle")
 tim.color("SeaGreen2")
-# def turn_right():
-#     tim.right(90)
-#     tim.forward(100)
 
-# turn_right()
-# turn_right()
-# turn_right()
-# turn_right()
-
-for _ in range(4):
-    tim.right(90)
-    tim.forward(100)
+for _ in range(20):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 
 
