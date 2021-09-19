28a78b4c1ba2b7d40a1fd7e0a5e25f16ef7e93d6
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-19 20:45:54 +0800

    tuples

--- OOP/turtle gui/main.py
+++ OOP/turtle gui/main.py
@@ -1,15 +1,23 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-tim = Turtle()
-screen = Screen()
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = (r, g, b)
+    return rgb
+
+
 color = ["MediumAquamarine", "LightCoral", "Tomato", "LightBlue", "MistyRose","DarkOliveGreen", "LightGreen", "SlateBlue", "RoyalBlue"]
 directions = [0,90,180,270]
 tim.speed('fastest')
 tim.pensize(10)
 
 for _ in range(250):
-    tim.color(random.choice(color))
+    tim.color(random_color())
     direction = random.choice(directions)
     tim.seth(direction)
     tim.fd(25)
---
+++ OOP/turtle gui/tuple.py
@@ -0,0 +1,2 @@
+my_tuple = (1,3,8)
+print(my_tuple[2])
