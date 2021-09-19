bfb2598f1fe3dc5cbf798e600b34fa669f26545e
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-09-19 15:29:51 +0800

    turtle challenge 3 -  drawing different shapes

--- OOP/turtle gui/main.py
+++ OOP/turtle gui/main.py
@@ -1,27 +1,19 @@
 from turtle import Turtle, Screen
-from random import randint
+import random
 
 tim = Turtle()
 screen = Screen()
+color = ["MediumAquamarine", "LightCoral", "Tomato", "LightBlue", "MistyRose", "DarkOrange"]
 
-def draw_shape(shape_angle, sides):
-    r = randint(0,255)
-    g = randint(0,255)
-    b = randint(0,255)
-    screen.colormode(255)
-    tim.pencolor(r,g,b)
+def draw_shape(sides):
+    shape_angle = 360 / sides
     for _ in range(sides):
         tim.right(shape_angle)
         tim.forward(100)
 
-draw_shape(120,3)
-draw_shape(90,4)
-draw_shape(72,5)
-draw_shape(60,6)
-draw_shape(51.42, 7)
-draw_shape(45, 8)
-draw_shape(40, 9)
-draw_shape(36, 10)
+for n in range(3,11):
+    tim.color(random.choice(color))
+    draw_shape(n)
 
 # tim.right(120)
 # tim.forward(100)
