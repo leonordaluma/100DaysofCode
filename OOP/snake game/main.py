d9118c3c58138e86e84044d0d422a47550bb2548
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-04 19:23:05 +0800

    snake challenge - detect collisions with tail

--- OOP/snake game/main.py
+++ OOP/snake game/main.py
@@ -30,11 +30,19 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         score.increase_score()
     
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         score.game_over()
+    
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 
 screen.exitonclick()
