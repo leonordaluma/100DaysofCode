d64734ecaca15c39450e7ff424dd4f3fa253c778
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-04 19:05:32 +0800

    snake challenge - scoreboard

---
+++ OOP/snake game/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Consolas", 12, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update_score()
+        self.hideturtle()
+    
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+    
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_score()
--- OOP/snake game/tempCodeRunnerFile.py
+++ OOP/snake game/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-screen.update()
+self.write(f"Score: {self.score}", align="center", font=("Consolas", 12, "normal"))
