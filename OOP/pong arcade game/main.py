12f6f62a2d0fbe1977308c9e13f35b178e2ccd58
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-05 20:25:53 +0800

    pong game - create second paddle

--- OOP/pong arcade game/main.py
+++ OOP/pong arcade game/main.py
@@ -7,11 +7,14 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-padle = Padle()
+r_paddle = Padle(350, 0)
+l_paddle = Padle(-350, 0)
 screen.listen()
 
-screen.onkey(padle.go_up, "Up")
-screen.onkey(padle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
--- OOP/pong arcade game/padle.py
+++ OOP/pong arcade game/padle.py
@@ -1,13 +1,13 @@
 from turtle import Turtle
 
 class Padle(Turtle):
-    def __init__(self):
+    def __init__(self, x_pos, y_pos):
         super().__init__()
         self.shape("square")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.color("white")
         self.penup()
-        self.setpos(350, 0)
+        self.setpos(x_pos, y_pos)
     
 
     def go_up(self):
