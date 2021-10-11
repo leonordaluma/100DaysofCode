5aa0d026a55a2b2c3faa7a5f2d1bf3f00586f3e2
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-11 19:10:43 +0800

    snake game - permanent highscore

--- OOP/snake game/scoreboard.py
+++ OOP/snake game/scoreboard.py
@@ -1,5 +1,5 @@
 from time import sleep
-from turtle import Turtle
+from turtle import Turtle, width
 ALIGNMENT = "center"
 FONT = ("Consolas", 12, "normal")
 
@@ -9,13 +9,14 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.update_score()
         self.hideturtle()
-    
+
     def update_score(self):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
@@ -23,9 +24,12 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.highscore))
         self.score = 0
         self.update_score()
     
     def increase_score(self):
         self.score += 1
         self.update_score()
+    
