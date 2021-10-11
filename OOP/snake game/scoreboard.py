4532567cd242001e992c9a0150c497e1bee8b381
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-11 18:43:26 +0800

    open, read, and write to files

--- OOP/snake game/scoreboard.py
+++ OOP/snake game/scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,13 +17,15 @@ class Scoreboard(Turtle):
         self.hideturtle()
     
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-    
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_score()
     
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_score()
