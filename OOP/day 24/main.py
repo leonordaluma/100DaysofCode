f9414438b02766b082c2993b7d6d28dc3c89ee9a
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-11 21:43:12 +0800

    python file paths

--- OOP/day 24/main.py
+++ OOP/day 24/main.py
@@ -3,5 +3,8 @@
 #     print(contents)
 
 
-with open("new_file.txt", mode="a") as file:
-    file.write(" General Kenobi.")
+# with open("C:/Users/msu-wone/Desktop/new_file.txt") as file:
+#     print(file.read())
+
+with open("../../../../Desktop/new_file.txt") as file:
+    print(file.read())
