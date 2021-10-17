3e1f51b1af7f991ac98ff60552d4eebf717718a2
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-17 10:11:54 +0800

    us states game - challenge 1

--- US states game/main.py
+++ US states game/main.py
@@ -1,13 +1,38 @@
+from scoreboard import Scoreboard
 import turtle
+import pandas
 
 screen = turtle.Screen()
+screen.setup(width=725, height=491)
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
+st = turtle.Turtle()
+st.hideturtle()
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+data = pandas.read_csv("50_states.csv")
+total_states = len(data.state)
+score = 0
+correct_guesses = []
 
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
+def write_guess(guess):
+    st.penup()
+    sf = data[data.state == guess]
+    x_cor = int(sf.x)
+    y_cor = int(sf.y)
+    st.goto(x_cor, y_cor)
+    st.write(guess)
+    correct_guesses.append(guess)
+
+is_game_on = True
+while is_game_on:
+    guess = screen.textinput(title=f"{score}/{total_states} States Correct", prompt="What's another state's name?").title()
+    for s in data.state:
+        if guess == s:
+            write_guess(guess)
+            score += 1
+            
+            
+
+        
---
+++ US states game/scoreboard.py
@@ -0,0 +1,6 @@
+import turtle
+
+class Scoreboard(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        
