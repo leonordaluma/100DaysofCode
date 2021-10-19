9d6fa27b8539caa011d6361adbc5d2c12cae6392
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-19 19:08:56 +0800

    list comprehension with conditions

--- list comprehension/main.py
+++ list comprehension/main.py
@@ -3,5 +3,9 @@
 # new_list = [letter for letter in name]
 # print(new_list)
 
-new_list = [n * 2 for n in range(1,5)]
-print(new_list)
+# new_list = [n * 2 for n in range(1,5)]
+# print(new_list)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+uppercase_names = [name.upper() for name in names if len(name) > 5]
+print(uppercase_names)
