d7ef29a6aac279d880e33c271dd6080bb58b5ed4
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-20 20:21:11 +0800

    list comprehension - data overlap

--- list comprehension/main.py
+++ list comprehension/main.py
@@ -1,4 +1,11 @@
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+with open("file1.txt") as file1_numbers:
+    file1_string = file1_numbers.readlines()
+    file1_int = [int(num) for num in file1_string]
 
-result = [num for num in numbers if num % 2 == 0]
+with open("file2.txt") as file2_numbers:
+    file2_string = file2_numbers.readlines()
+    file2_int = [int(num) for num in file2_string]
+
+result = [num for num in file1_int if num in file2_int]
 print(result)
+
