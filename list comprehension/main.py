aec1b6a96cb9e98d19fe5188f8bcd424eeedab51
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-20 19:54:44 +0800

    list comprehension - filtering even numbers

--- list comprehension/main.py
+++ list comprehension/main.py
@@ -1,4 +1,4 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-squared_numbers = [num * num for num in numbers]
-print(squared_numbers)
+result = [num for num in numbers if num % 2 == 0]
+print(result)
