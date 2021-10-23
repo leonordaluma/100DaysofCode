0b7014aa9bc317fe637db608cfb89b032da0d4a9
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-23 18:44:24 +0800

    day 26

--- list comprehension/main.py
+++ list comprehension/main.py
@@ -4,4 +4,13 @@ alphabets = pandas.read_csv("nato_phonetic_alphabet.csv")
 # print(type(alphabets))
 
 nato_alphabets_dict = {row.letter: row.code for (index, row) in alphabets.iterrows()}
-print(nato_alphabets_dict)
+
+user_input = input("Enter a word: ").upper()
+# code_words = []
+# for index in range(len(user_input)):
+#     letter = user_input[index]
+#     if letter in nato_alphabets_dict.keys():
+#         code_words.append(nato_alphabets_dict[letter])
+code_words = [nato_alphabets_dict[letter] for letter in user_input]
+
+print(code_words)
