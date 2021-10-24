8a4ac34ba91bc07327b13851f21e2f37e53430e0
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-24 21:34:53 +0800

    **kwargs

--- tkinter/playground.py
+++ tkinter/playground.py
@@ -2,6 +2,21 @@ def add(*numbers):
     sum = 0
     for n in numbers:
         sum += n
-    print(sum)
+    return sum
 
-add(10,7,1,2)
+# add(10,7,1,2)
+
+def calculate(**kwargs):
+    print(kwargs)
+
+calculate(num1 = 1, num2 = 2)
+
+class Car:
+
+    def __init__(self, **kwargs):
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+
+my_car = Car(make="Tesla")
+print(my_car.make)
+print(my_car.model)
