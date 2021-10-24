3a0f33a3830b9279a3e069f2b2b34357d41014cb
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-24 22:29:21 +0800

    tkinter configure properties

--- tkinter/main.py
+++ tkinter/main.py
@@ -5,7 +5,9 @@ window.title("GUI")
 window.minsize(width=500, height=300)
 
 # label 
-my_lable = tkinter.Label(text="Label", font=("Poppins", 14))
-my_lable.pack(side="left")
+my_label = tkinter.Label(text="Label", font=("Poppins", 14))
+my_label.pack(side="left")
 
+my_label["text"] = "New Label"
+my_label.config(text="New Text")
 window.mainloop()
