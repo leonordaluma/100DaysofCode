2ae6d928c6eddedf97a0676c8412c5006cc4aa2c
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-24 22:37:47 +0800

    tkinter button

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,13 +1,24 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("GUI")
 window.minsize(width=500, height=300)
 
 # label 
-my_label = tkinter.Label(text="Label", font=("Poppins", 14))
-my_label.pack(side="left")
+my_label = Label(text="Label", font=("Poppins", 14))
+my_label.pack()
 
 my_label["text"] = "New Label"
 my_label.config(text="New Text")
+
+# buttons
+def button_clicked():
+    my_label.config(text="Button Got Clicked!")
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+
+
 window.mainloop()
