f3052d31cf3cbd3654a248d18149135e5da11fac
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-10-26 16:20:18 +0800

    tkinter layout managers

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,78 +1,31 @@
 from tkinter import *
 
+def button_clicked():
+    new_text = entry.get()
+    my_label.config(text=new_text)
+
 window = Tk()
 window.title("GUI")
-window.minsize(width=500, height=500)
+window.minsize(width=500, height=300)
+window.config(padx=50, pady=50)
 
 # label 
 my_label = Label(text="Label", font=("Poppins", 14))
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 my_label["text"] = "New Label"
 my_label.config(text="New Text")
 
 # buttons
-def button_clicked():
-    new_text = entry.get()
-    my_label.config(text=new_text)
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+button.config(padx=100, pady=20)
 
+button2 = Button(text="New Button")
+button2.grid(column=2, row=0)
 # entry
-entry = Entry(width=30)
-entry.insert(END, string="Some text to begin with")
-entry.pack()
-
-# textbox
-text = Text(height=5, width=30)
-text.focus()
-text.insert(END,"this is a text")
-# line 1 character 0
-print(text.get("1.0", END))
-text.pack()
-
-# Spinbox
-def spinbox_value():
-    print(spinbox.get())
-spinbox = Spinbox(from_= 0, to=10, width=5, command=spinbox_value)
-spinbox.pack()
-
-# Scale
-scale = Scale(from_=0, to=20)
-scale.pack()
-
-# Checkbutton
-def checkbutton_used():
-    print(check_state.get())
-check_state = IntVar()
-checkbutton = Checkbutton(text="Is On?", variable=check_state, command=checkbutton_used)
-checkbutton.pack()
-
-# Radiobutton
-def radiobutton_value():
-    print(radio_state.get())
-
-radio_state = IntVar()
-r_btn1 = Radiobutton(text="True", value=1, variable=radio_state, command=radiobutton_value)
-r_btn2 = Radiobutton(text="False", value=2, variable=radio_state, command=radiobutton_value)
-r_btn1.pack()
-r_btn2.pack()
-
-# Listbox
-def listbox_used(event):
-    print(listbox.get(listbox.curselection()))
-
-listbox = Listbox(height=3)
-cats = ["Grey", "Harley", "Mufasa"]
-for c in cats:
-    listbox.insert(cats.index(c), c)
-listbox.bind("<<ListboxSelect>>", listbox_used)
-listbox.pack()
-
-
-
-
-
+entry = Entry(width=10)
+entry.grid(column=3, row=2)
 
 window.mainloop()
