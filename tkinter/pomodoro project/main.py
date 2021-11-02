285e1d3a37bbba519a5dba3a5902a04651cad2a7
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-02 19:29:30 +0800

    pomodoro - different timer sessions and values

--- tkinter/pomodoro project/main.py
+++ tkinter/pomodoro project/main.py
@@ -9,12 +9,26 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN
+    short_break_sec = SHORT_BREAK_MIN 
+    long_break_sec = LONG_BREAK_MIN
+
+    if reps % 8 == 0:
+        timer.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timer.config(text="Work", fg=GREEN)
+        count_down(work_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -25,6 +39,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
