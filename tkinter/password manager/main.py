0efb3fb7cd0147879fb4bc5353eb7650ecee2d43
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-04 16:33:15 +0800

    password manager - columnspan

--- tkinter/password manager/main.py
+++ tkinter/password manager/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+FONT = ("Arial", 14, "normal")
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -12,7 +13,31 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+website_text = Label(text="Website: ")
+website_text.grid(row=1, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_text = Label(text="Email/Username:")
+email_text.grid(row=2, column=0)
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_text = Label(text="Password: ")
+password_text.grid(row=3, column=0)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_btn = Button(text="Generate Password")
+generate_btn.grid(row=3, column=2)
+
+add_btn = Button(text="Add", width=36)
+add_btn.grid(row=4, column=1, columnspan=2)
 
 
 
