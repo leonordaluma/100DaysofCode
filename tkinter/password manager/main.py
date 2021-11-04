c9f6c58dfd3bf9ebb8a7e2e17d7c457bc578a7f6
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-04 17:18:05 +0800

    password manager - grid layout

--- tkinter/password manager/main.py
+++ tkinter/password manager/main.py
@@ -8,7 +8,7 @@ FONT = ("Arial", 14, "normal")
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
@@ -19,13 +19,13 @@ website_text = Label(text="Website: ")
 website_text.grid(row=1, column=0)
 
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1, columnspan=3)
 
 email_text = Label(text="Email/Username:")
 email_text.grid(row=2, column=0)
 
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.grid(row=2, column=1, columnspan=3)
 
 password_text = Label(text="Password: ")
 password_text.grid(row=3, column=0)
