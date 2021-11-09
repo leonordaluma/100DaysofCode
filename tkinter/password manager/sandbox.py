907cccd467a9aa724906a81f8ebb87f1d5a1dfa5
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-10 06:41:51 +0800

    KeyError handling

--- tkinter/password manager/sandbox.py
+++ tkinter/password manager/sandbox.py
@@ -1,15 +1,21 @@
-fruits = ["Appke", "Pear", "Orange"]
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3},
+]
 
 
-def make_pie(index):
+total_likes = 0
+
+for post in facebook_posts:
     try:
-        fruit = fruits[index]
-    except IndexError:
-        print("Fruit pie")
-    else:
-        print(fruit + " pie")
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        # total_likes += 0
+        pass
+        
 
-
-
-make_pie(4)
-make_pie(2)
+print(total_likes)
