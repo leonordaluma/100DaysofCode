4bb7f2abbdbc2e3788e0c6209138e40ba36398cf
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-09 21:26:07 +0800

    catch specific exception

--- tkinter/password manager/sandbox.py
+++ tkinter/password manager/sandbox.py
@@ -2,7 +2,9 @@ try:
     file = open("some_file.txt")
     a_dict = {"key":"value"}
     print(a_dict["sssss"])
-except:
+except FileNotFoundError:
     file = open("some_file.txt", "w")
     file.write("Something")
     # print("File does not exist.")
+except KeyError:
+    print("Value does not exist.")
