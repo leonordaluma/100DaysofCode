df34e35ea62f6c03a313255a0776cbf00146fdb4
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-09 21:30:38 +0800

    try else

--- tkinter/password manager/sandbox.py
+++ tkinter/password manager/sandbox.py
@@ -1,10 +1,13 @@
 try:
     file = open("some_file.txt")
     a_dict = {"key":"value"}
-    print(a_dict["sssss"])
+    print(a_dict["key"])
 except FileNotFoundError:
     file = open("some_file.txt", "w")
     file.write("Something")
     # print("File does not exist.")
 except KeyError as error_message:
     print(f"{error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
