83ebf1ae56a324804716ea6119ad96b585b9861f
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-10 07:02:48 +0800

    nato alphabets exception handling

--- list comprehension/main.py
+++ list comprehension/main.py
@@ -1,16 +1,29 @@
 import pandas
 
 alphabets = pandas.read_csv("nato_phonetic_alphabet.csv")
-# print(type(alphabets))
 
 nato_alphabets_dict = {row.letter: row.code for (index, row) in alphabets.iterrows()}
+print(nato_alphabets_dict)
 
-user_input = input("Enter a word: ").upper()
-# code_words = []
-# for index in range(len(user_input)):
-#     letter = user_input[index]
-#     if letter in nato_alphabets_dict.keys():
-#         code_words.append(nato_alphabets_dict[letter])
-code_words = [nato_alphabets_dict[letter] for letter in user_input]
+is_error = True
+while is_error:
+    try:
+        user_input = input("Enter a word: ").upper()
+        code_words = [nato_alphabets_dict[letter] for letter in user_input]
+        print(code_words)
+        is_error = False
+    except KeyError:
+        is_error = True
+        print("Sorry, only letters in the alphabet please.")
 
-print(code_words)
+# def generate_nato():
+#     user_input = input("Enter a word: ").upper()
+#     try:
+#         code_words = [nato_alphabets_dict[letter] for letter in user_input]
+#     except KeyError:
+#         print("Sorry, only letters in the alphabet please.")
+#         generate_nato()
+#     else:
+#         print(code_words)
+
+# generate_nato()
