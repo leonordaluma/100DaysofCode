436951974c5900dae90f10359b119f1fe0dc6bf3
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-09 21:27:01 +0800

    get hold of the error message

--- tkinter/password manager/sandbox.py
+++ tkinter/password manager/sandbox.py
@@ -6,5 +6,5 @@ except FileNotFoundError:
     file = open("some_file.txt", "w")
     file.write("Something")
     # print("File does not exist.")
-except KeyError:
-    print("Value does not exist.")
+except KeyError as error_message:
+    print(f"{error_message} does not exist.")
