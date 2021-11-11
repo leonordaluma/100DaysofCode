fcd4f3a9d6b8cde7583c040d9b296c4f0e12bdbd
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-11 17:57:24 +0800

    flash card app creating the UI

--- capstone projects/flash card app/main.py
+++ capstone projects/flash card app/main.py
@@ -1,20 +1,28 @@
 from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
+TITLE = ("Arial", 18, "italic")
+WORD = ("Arial", 30, "bold")
 
 window = Tk()
 window.title("Flash Card App")
-window.config(padx=50, bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=0, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=640, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400,300, image=card_img)
-canvas.grid(column=1, row=0, columnspan=2)
+french_text = canvas.create_text(400, 200, text="French", font=TITLE)
+french_word = canvas.create_text(400, 300, text="mettre", font=WORD)
+canvas.grid(column=0, row=0, columnspan=2)
 
 
-# my_image = PhotoImage(file="./images/wrong.png")
-# x_btn = Button(image=my_image, highlightthickness=0)
-# x_btn.pack()
 
+wrong_image = PhotoImage(file="./images/wrong.png")
+wrong_btn = Button(image=wrong_image, highlightthickness=0)
+wrong_btn.grid(column=0, row=1)
+
+right_image = PhotoImage(file="./images/right.png")
+right_btn = Button(image=right_image, highlightthickness=0)
+right_btn.grid(column=1, row=1)
 
 
 window.mainloop()
