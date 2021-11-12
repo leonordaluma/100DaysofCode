f08d58c9f0b303a35244db7c52d5bf372d566f6a
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-12 16:59:59 +0800

    flash card app generate new word

--- capstone projects/flash card app/main.py
+++ capstone projects/flash card app/main.py
@@ -1,7 +1,20 @@
 from tkinter import *
+import pandas as pd
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
 TITLE = ("Arial", 18, "italic")
 WORD = ("Arial", 30, "bold")
+
+
+def generate_word():
+    data = pd.read_csv("./data/french_words.csv")
+    data_dict = data.to_dict(orient='records')
+    random_word = random.choice(data_dict)
+    french_word = random_word["French"]
+    # english_word = random_word["English"]
+    canvas.itemconfig(french_text, text=f"{french_word}")
+
 
 window = Tk()
 window.title("Flash Card App")
@@ -11,17 +24,17 @@ canvas = Canvas(width=800, height=640, bg=BACKGROUND_COLOR, highlightthickness=0
 card_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400,300, image=card_img)
 french_text = canvas.create_text(400, 200, text="French", font=TITLE)
-french_word = canvas.create_text(400, 300, text="mettre", font=WORD)
+french_text = canvas.create_text(400, 300, text=f"", font=WORD)
 canvas.grid(column=0, row=0, columnspan=2)
 
 
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_btn = Button(image=wrong_image, highlightthickness=0)
+wrong_btn = Button(image=wrong_image, highlightthickness=0, command=generate_word)
 wrong_btn.grid(column=0, row=1)
 
 right_image = PhotoImage(file="./images/right.png")
-right_btn = Button(image=right_image, highlightthickness=0)
+right_btn = Button(image=right_image, highlightthickness=0, command=generate_word)
 right_btn.grid(column=1, row=1)
 
 
