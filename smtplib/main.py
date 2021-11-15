021d9bbef7bc6a4a2eaa3f1b75eddfcb1f640a2b
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-15 22:17:50 +0800

    send random quote to self

--- smtplib/main.py
+++ smtplib/main.py
@@ -1,26 +1,19 @@
-# import smtplib
-
-# my_email = 'name@email.com'
-# password = 'password123'
-
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="receiver@yahoo.com",
-#         msg="Subject:New Message\n\nThis is the body of my email."
-#     )
-
+import smtplib
 import datetime as dt
+import random
 
-now = dt.datetime.now()
-print(now)
-year = now.year
-print(year)
-month = now.month
-day_of_week = now.weekday()
-print(day_of_week)
+current_day = dt.datetime.weekday()
 
-dob = dt.datetime(year=1999, month=5, day=12)
-print(dob.year)
+with open("quotes.txt") as file:
+    data = file.readlines()
+    quote = random.choice(data)
+
+my_email = "myemail@gmail.com"
+my_pd = "pass1234"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject:Good morning\n\n\n{quote}")
+
+
+
