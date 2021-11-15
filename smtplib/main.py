36f5751649484e6f2ddabd25707a2c868de24e48
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-15 22:06:20 +0800

    datetime python module

--- smtplib/main.py
+++ smtplib/main.py
@@ -1,13 +1,26 @@
-import smtplib
+# import smtplib
 
-my_email = 'name@email.com'
-password = 'password123'
+# my_email = 'name@email.com'
+# password = 'password123'
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="receiver@yahoo.com",
-        msg="Subject:New Message\n\nThis is the body of my email."
-    )
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="receiver@yahoo.com",
+#         msg="Subject:New Message\n\nThis is the body of my email."
+#     )
+
+import datetime as dt
+
+now = dt.datetime.now()
+print(now)
+year = now.year
+print(year)
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+dob = dt.datetime(year=1999, month=5, day=12)
+print(dob.year)
