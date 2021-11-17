911f784aeab50e5504c470fd14545c41ede9bf6d
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-17 13:16:27 +0800

    smtplib challenge 1

--- smtplib/main.py
+++ smtplib/main.py
@@ -2,18 +2,24 @@ import smtplib
 import datetime as dt
 import random
 
-current_day = dt.datetime.weekday()
+current_day = dt.datetime.now()
+weekday = current_day.weekday()
+my_email = ""
+my_pd = ""
 
-with open("quotes.txt") as file:
-    data = file.readlines()
-    quote = random.choice(data)
 
-my_email = "myemail@gmail.com"
-my_pd = "pass1234"
+if weekday == 2:
+    with open("quotes.txt") as file:
+        data = file.readlines()
+        quote = random.choice(data)
+    
+    print(quote)
+
 
 with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
-    connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject:Good morning\n\n\n{quote}")
+    connection.login(user=my_email, password=my_pd)
+    connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject:Good morning\n\n{quote}")
 
 
 
