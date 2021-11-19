5e4818cbe6d3e3ba713e36c2318df9e627a5b7d6
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-19 21:25:45 +0800

    sunset, sunrise challenge

--- api projects/main.py
+++ api projects/main.py
@@ -1,8 +1,20 @@
 import requests
+from datetime import *
 
+LAT = 14.599512
+LONG = 120.984222
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
-
 data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+now = datetime.now()
+print(sunrise)
+print(now)
