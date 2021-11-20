0e66cbc6ad4d3698c4b4965625b4b6d79d0a6fb1
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-20 12:32:24 +0800

    iss notifier start

--- api projects/main.py
+++ api projects/main.py
@@ -1,8 +1,19 @@
 import requests
 from datetime import *
 
-LAT = 14.599512
-LONG = 120.984222
+from requests.api import request
+
+LAT = 23.697809
+LONG = 120.960518
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_lat = float(data["iss_position"]["latitude"])
+iss_long = float(data["iss_position"]["longitude"])
+
+# Your position is +5 or -5 degrees of the ISS position 
 
 parameters = {
     "lat": LAT,
@@ -13,9 +24,17 @@ parameters = {
 response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 now = datetime.now()
-print(sunrise)
-print(sunset)
-print(now.hour)
+
+
+print(data["results"]["sunrise"])
+print(data["results"]["sunset"])
+print(now)
+
+
+# if the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up 
+# BONUS: run code every 60 seconds 
