e50d29263e0d56491f7c785a865bd024f3e77ae9
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-20 13:01:40 +0800

    compare position

--- api projects/main.py
+++ api projects/main.py
@@ -1,37 +1,38 @@
 import requests
 from datetime import *
 
-from requests.api import request
-
 LAT = 23.697809
 LONG = 120.960518
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-iss_lat = float(data["iss_position"]["latitude"])
-iss_long = float(data["iss_position"]["longitude"])
+    iss_lat = float(data["iss_position"]["latitude"])
+    iss_long = float(data["iss_position"]["longitude"])
+    
+    if LAT - 5 <= iss_lat <= LAT + 5 and LONG-5 <= iss_long <= LONG+5:
+        return True
 
-# Your position is +5 or -5 degrees of the ISS position 
+def is_night():
+    parameters = {
+        "lat": LAT,
+        "lng": LONG,
+        "formatted": 0,
+    }
 
-parameters = {
-    "lat": LAT,
-    "lng": LONG,
-    "formatted": 0,
-}
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    
+    if 
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+
 now = datetime.now()
-
-
-print(data["results"]["sunrise"])
-print(data["results"]["sunset"])
-print(now)
 
 
 # if the ISS is close to my current position
