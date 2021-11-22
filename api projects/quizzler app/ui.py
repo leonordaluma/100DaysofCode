2773461c2d8993f6d277c3c80da09e2b4809ca35
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-23 07:41:24 +0800

    quizzler score label

--- api projects/quizzler app/ui.py
+++ api projects/quizzler app/ui.py
@@ -1,12 +1,16 @@
 from tkinter import *
 THEME_COLOR = "#375362"
-
+QUESTION_FONT = ("Arial", 20, "Italic")
 class QuizInterface:
     
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler App")
-        self.window.config(background=THEME_COLOR)
+        self.window.config(background=THEME_COLOR, padx=20, pady=20)
+        self.score = 0
+        
+        self.score_text = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white")
+        self.score_text.grid(row=0, column=1)
         
         
         self.window.mainloop()
