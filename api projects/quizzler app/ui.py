ea7966ea5ba9bb45e769cbe7cdc7cd9d7f2edb1f
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-23 07:57:50 +0800

    canvas + padding + font

--- api projects/quizzler app/ui.py
+++ api projects/quizzler app/ui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 THEME_COLOR = "#375362"
-QUESTION_FONT = ("Arial", 20, "Italic")
+QUESTION_FONT = ("Arial", 14, "italic")
 class QuizInterface:
     
     def __init__(self):
@@ -8,6 +8,12 @@ class QuizInterface:
         self.window.title("Quizzler App")
         self.window.config(background=THEME_COLOR, padx=20, pady=20)
         self.score = 0
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.q_text = self.canvas.create_text(150,125, 
+                                         text="Amazon acquired Twitch in August 2014 for $970 million dollars.", 
+                                         font=QUESTION_FONT,
+                                         width=280)
+        self.canvas.grid(row=1, column=0, columnspan=2)
         
         self.score_text = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white")
         self.score_text.grid(row=0, column=1)
