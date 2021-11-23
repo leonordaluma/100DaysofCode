306942e2cf19dd570486ac474e51102b43d192a8
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-23 08:12:55 +0800

    adjustments

--- api projects/quizzler app/ui.py
+++ api projects/quizzler app/ui.py
@@ -12,19 +12,20 @@ class QuizInterface:
         self.q_text = self.canvas.create_text(150,125, 
                                          text="Amazon acquired Twitch in August 2014 for $970 million dollars.", 
                                          font=QUESTION_FONT,
+                                         fill=THEME_COLOR,
                                          width=280)
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         
         self.score_text = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white")
-        self.score_text.grid(row=0, column=1)
+        self.score_text.grid(row=0, column=1, padx=50)
         
-        self.true_image = PhotoImage(file="./images/true.png")
-        self.false_image = PhotoImage(file="./images/false.png")
+        true_image = PhotoImage(file="./images/true.png")
+        false_image = PhotoImage(file="./images/false.png")
         
-        self.true_button = Button(image=self.true_image, highlightthickness=0, bg=THEME_COLOR)
+        self.true_button = Button(true_image, highlightthickness=0, bg=THEME_COLOR)
         self.true_button.grid(row=2, column=0)
         
-        self.false_button = Button(image=self.false_image, highlightthickness=0, bg=THEME_COLOR)
+        self.false_button = Button(false_image, highlightthickness=0, bg=THEME_COLOR)
         self.false_button.grid(row=2, column=1)
         
         
