87bae93ed1942947f53a10a6ab96a94c1c0d09cb
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-24 22:26:51 +0800

    api parameters

--- api projects/rain alert/main.py
+++ api projects/rain alert/main.py
@@ -1,1 +1,18 @@
 from api_key import API_KEY
+import requests
+
+LAT = 14.599512
+LONG = 120.984222
+
+parameters = {
+    "lat" : LAT,
+    "long": LONG,
+    "appid": API_KEY,
+    
+}
+
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data["current"])
