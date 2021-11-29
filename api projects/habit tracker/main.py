e8b117d72d673abda8beb75a8a971cc84d829700
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-29 18:29:49 +0800

    habit tracker - graph endpoint + params

--- api projects/habit tracker/main.py
+++ api projects/habit tracker/main.py
@@ -2,13 +2,31 @@ from keys import token
 import requests
 
 
+TOKEN = token
+USERNAME = "stormpo0per"
+
 pixela_url = "https://pixe.la/v1/users"
 user_parameters = {
-    "token": token,
-    "username": "stormpo0per",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-r = requests.post(url=pixela_url, json=user_parameters)
-print(r.text)
+# r = requests.post(url=pixela_url, json=user_parameters)
+# print(r.text)
+
+graphs_url = f"{pixela_url}/{USERNAME}/graphs"
+graph_parameters = {
+    "id": "mygraph",
+    "name": "Reading Graph",
+    "unit": "Pages",
+    "type": "int",
+    "color": "shibafu",
+    "timezone": "Asia/Tokyo"
+}
+
+headers = {
+    "X-USER-TOKEN" : TOKEN
+}
+
