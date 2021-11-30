325bffe107437c858b9a2f474375892868df7b0d
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-11-30 08:09:08 +0800

    step 4 saving data into gsheet

--- api projects/workout tracking/main.py
+++ api projects/workout tracking/main.py
@@ -1,5 +1,7 @@
 from keys import app_id, api_key
+from datetime import datetime
 import requests
+
 
 GENDER = "female"
 WEIGHT =  45
@@ -14,6 +16,8 @@ HEADERS = {
 input_text = input("Tell me which exercise you did: ")
 
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/af73963297d466e6b7310d8445045a09/workoutTracking/workouts"
+
 parameters = {
     "query": input_text,
     "gender": GENDER,
@@ -24,4 +28,18 @@ parameters = {
 
 r = requests.post(url=nutritionix_endpoint, json=parameters, headers=HEADERS)
 result = r.json()
-print(result)
+exercises = result["exercises"]
+date = datetime.now().strftime("%d/%m/%Y")
+time = datetime.now().strftime("%X")
+for exercise in exercises:
+    sheety_parameters = {
+        "workout" : {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+res = requests.post(url=sheety_endpoint, json=sheety_parameters)
+print(res.text)
