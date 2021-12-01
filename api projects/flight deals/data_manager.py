4619d8871f876390e98a64d5d7963f255cbc3cde
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-01 22:16:37 +0800

    data_manager start

--- api projects/flight deals/data_manager.py
+++ api projects/flight deals/data_manager.py
@@ -1,3 +1,9 @@
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.sheet_endpoint = ""
+        self.sheet_parameters = {
+            
+        }
+        
     pass
