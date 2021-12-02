df3fdd1838a15044155d4bda8d6b1235ba4cf254
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-02 21:02:23 +0800

    flight deals - sheety endpoint

--- api projects/flight deals/data_manager.py
+++ api projects/flight deals/data_manager.py
@@ -1,9 +1,15 @@
+from requests.api import request
+from pprint import pprint
+
+SHEETY_ENDPOINT = "https://api.sheety.co/af73963297d466e6b7310d8445045a09/flightDeals/prices"
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.sheet_endpoint = ""
-        self.sheet_parameters = {
-            
-        }
+        self.destination_data = {}
         
-    pass
+    def get_destination_data(self):
+        r = request.get(url=SHEETY_ENDPOINT)
+        data = r.json()
+        self.destination_data = data["prices"]
+        pprint(data)
+        return self.destination_data
--- api projects/flight deals/main.py
+++ api projects/flight deals/main.py
@@ -1,1 +1,6 @@
+from data_manager import DataManager
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+print(sheet_data)
