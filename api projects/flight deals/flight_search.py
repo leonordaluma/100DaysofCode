70be0dd3def0478cb811685a314e5773fa6614ca
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-02 21:30:29 +0800

    get destination codes

--- api projects/flight deals/flight_search.py
+++ api projects/flight deals/flight_search.py
@@ -1,8 +1,25 @@
+from requests.api import head
 from keys import api_key
 import requests
 
 
 ENDPOINT = "https://tequila-api.kiwi.com"
 API_KEY = api_key
+
+
 class FlightSearch:
-    
+
+    def get_destination_code(self, city_name):
+        location_endpoint = f"{ENDPOINT}/locations/query"
+        header = {
+            "apikey": API_KEY
+        }
+        parameters = {
+            "term": city_name,
+            "location_types": "city"
+        }
+        r = requests.get(url=location_endpoint,
+                         params=parameters, headers=header)
+        results = r.json()["locations"]
+        code = results[0]["code"]
+        return code
--- api projects/flight deals/main.py
+++ api projects/flight deals/main.py
@@ -1,7 +1,13 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
 from pprint import pprint
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
 data_manager = DataManager()
+flight_search = FlightSearch()
+
 sheet_data = data_manager.get_destination_data()
+for data in sheet_data["city"]:
+    f_data = flight_search.get_destination_code("Paris")
+
 pprint(sheet_data)
