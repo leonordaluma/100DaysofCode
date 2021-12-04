6775ff7436d646a0dbc693d85085f02c31aa5f98
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-04 19:11:01 +0800

    day 39

--- api projects/flight deals/main.py
+++ api projects/flight deals/main.py
@@ -1,11 +1,13 @@
 from data_manager import DataManager
+from notification_manager import NotificationManager
 from flight_search import FlightSearch
 from datetime import datetime, time, timedelta
 from pprint import pprint
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
 ORIGIN_CITY_IATA = "PR"
 data_manager = DataManager()
+notification_manager = NotificationManager()
 flight_search = FlightSearch()
 sheet_data = data_manager.get_destination_data()
 
@@ -20,10 +22,15 @@ six_months_from_today = datetime.now() + timedelta(days=(30 * 6))
 
 for destination in sheet_data:
     flight = flight_search.check_flights(
-        origin_city_code= ORIGIN_CITY_IATA,
-        destination_city_code= destination["iataCode"],
-        from_time= tomorrow,
-        to_time= six_months_from_today        
+        origin_city_code=ORIGIN_CITY_IATA,
+        destination_city_code=destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months_from_today
     )
 
-pprint(flight)
+    if flight is not None and flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
+
+# pprint(flight)
