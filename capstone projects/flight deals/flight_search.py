86990e9c26df3bb0244c0e6672d6499f76a964d1
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-06 15:10:14 +0800

    flight_search

--- capstone projects/flight deals/flight_search.py
+++ capstone projects/flight deals/flight_search.py
@@ -1,37 +1,24 @@
+import requests
 from flight_data import FlightData
-from pprint import pprint
-import requests
-from keys import api_key
-import requests
 
-
-
-ENDPOINT = "https://tequila-api.kiwi.com"
-API_KEY = api_key
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = YOUR FLIGHT SEARCH API KEY
 
 
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        location_endpoint = f"{ENDPOINT}/locations/query"
-        header = {
-            "apikey": API_KEY
-        }
-        parameters = {
-            "term": city_name,
-            "location_types": "city"
-        }
-        r = requests.get(url=location_endpoint,
-                         params=parameters, headers=header)
-        results = r.json()["locations"]
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
         code = results[0]["code"]
         return code
 
     def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
-        headers = {
-            "apikey": API_KEY
-        }
-        parameters = {
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
             "fly_from": origin_city_code,
             "fly_to": destination_city_code,
             "date_from": from_time.strftime("%d/%m/%Y"),
@@ -41,13 +28,38 @@ class FlightSearch:
             "flight_type": "round",
             "one_for_city": 1,
             "max_stopovers": 0,
-            "curr": "PHP" 
+            "curr": "GBP"
         }
-        
-        r = requests.get(url=f"{ENDPOINT}/v2/search", headers=headers, params=parameters)
-        raw_data = r.json()["data"]
-        if raw_data:
-            data = raw_data[0]
+
+        response = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/v2/search",
+            headers=headers,
+            params=query,
+        )
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            query["max_stopovers"] = 1
+            response = requests.get(
+                url=f"{TEQUILA_ENDPOINT}/v2/search",
+                headers=headers,
+                params=query,
+            )
+            data = response.json()["data"][0]
+            pprint(data)
+            flight_data = FlightData(
+                price=data["price"],
+                origin_city=data["route"][0]["cityFrom"],
+                origin_airport=data["route"][0]["flyFrom"],
+                destination_city=data["route"][1]["cityTo"],
+                destination_airport=data["route"][1]["flyTo"],
+                out_date=data["route"][0]["local_departure"].split("T")[0],
+                return_date=data["route"][2]["local_departure"].split("T")[0],
+                stop_overs=1,
+                via_city=data["route"][0]["cityTo"]
+            )
+            return flight_data
+        else:
             flight_data = FlightData(
                 price=data["price"],
                 origin_city=data["route"][0]["cityFrom"],
@@ -57,7 +69,5 @@ class FlightSearch:
                 out_date=data["route"][0]["local_departure"].split("T")[0],
                 return_date=data["route"][1]["local_departure"].split("T")[0]
             )
-            print(f"{flight_data.destination_city}: P{flight_data.price}")
+
             return flight_data
-        else:
-            return None
