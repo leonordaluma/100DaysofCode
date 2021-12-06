ad2b3df417c469ee6124c3e2c3ee14d3b7c8a0d2
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-06 15:10:44 +0800

    notification_manager

--- capstone projects/flight deals/notification_manager.py
+++ capstone projects/flight deals/notification_manager.py
@@ -1,22 +1,34 @@
+import smtplib
 from twilio.rest import Client
-from keys import twilio_sid, twilio_auth_tokens, twilio_virtual_number, twilio_verified_number
 
-
-TWILIO_SID = twilio_sid
-TWILIO_AUTH_TOKENS = twilio_auth_tokens
-TWILIO_VIRTUAL_NUMBER = twilio_virtual_number
-TWILIO_VERIFIED_NUMBER = twilio_verified_number
-
+TWILIO_SID = YOUR TWILIO ACCOUNT SID
+TWILIO_AUTH_TOKEN = YOUR TWILIO AUTH TOKEN
+TWILIO_VIRTUAL_NUMBER = YOUR TWILIO VIRTUAL NUMBER
+TWILIO_VERIFIED_NUMBER = YOUR TWILIO VERIFIED NUMBER
+MAIL_PROVIDER_SMTP_ADDRESS = YOUR EMAIL PROVIDER SMTP ADDRESS "smtp.gmail.com"
+MY_EMAIL = YOUR EMAIL
+MY_PASSWORD = YOUR PASSWORD
 
 class NotificationManager:
+
     def __init__(self):
-        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKENS)
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
     def send_sms(self, message):
         message = self.client.messages.create(
-                body=message,
-                from_=TWILIO_VIRTUAL_NUMBER,
-                to=TWILIO_VERIFIED_NUMBER,
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
         )
-        # Prints if successfully sent.
         print(message.sid)
+
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
+                )
