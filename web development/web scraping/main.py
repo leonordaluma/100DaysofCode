1897ef59636b9c799837b041ce593d665783a71d
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-14 17:17:11 +0800

    get all titles, links, and upvotes

--- web development/web scraping/main.py
+++ web development/web scraping/main.py
@@ -6,13 +6,16 @@ res = requests.get("https://news.ycombinator.com/")
 contents = res.text
 
 soup = BeautifulSoup(contents, "html.parser")
-article_tag = soup.find(name="a", class_="titlelink")
-article_text = article_tag.getText()
-article_link = article_tag.get("href")
-article_score = soup.find(name="span", class_="score").getText()
-print(article_link)
-print(article_score)
-# a_elements = soup.find_all(name="a", class_="titlelink")
-
-# for a in a_elements:
-#     print(a)
+articles = soup.find_all(name="a", class_="titlelink")
+article_texts = []
+article_links = []
+for tag in articles:
+    text = tag.getText()
+    article_texts.append(text)
+    link = tag.get("href")
+    article_links.append(link)
+    
+article_scores = [score.getText() for score in soup.find_all(name="span", class_="score")]
+print(article_texts)
+print(article_links)
+print(article_scores)
