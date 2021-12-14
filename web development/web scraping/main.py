8dc1fe8c436575f563f9a378f765a8f1a141afc2
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-14 16:53:38 +0800

    get the website's content

--- web development/web scraping/main.py
+++ web development/web scraping/main.py
@@ -1,27 +1,6 @@
 from bs4 import BeautifulSoup
-import lxml
+import requests
 
-with open("website.html", encoding="utf8") as file:
-    contents = file.read() 
-
-
-soup = BeautifulSoup(contents, "html.parser")
-
-all_anchor_tags = soup.find_all(name="a")
-
-# for tag in all_anchor_tags:
-#     print(tag)
-#     print(tag.getText())
-#     print(tag.get("href"))
-
-# heading = soup.find(name="h1", id="name")
-# print(heading.name)
-# print(heading.getText())
-# print(heading.get("id"))
-# print(soup.find(name="h3", class_="heading" ))
-
-
-url = soup.select_one(selector="p em")
-print(url)
-print(url.getText())
+res = requests.get("https://news.ycombinator.com/")
+contents = res.text
 
