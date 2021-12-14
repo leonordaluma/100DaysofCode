9ff273b96e6ed041d72d938b042364b66e7449cb
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-14 15:16:04 +0800

    access html

--- web development/web scraping/main.py
+++ web development/web scraping/main.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import lxml
 
-with open("website.html") as file:
+with open("website.html", encoding="utf8") as file:
     contents = file.read() 
 
 
-soup = BeautifulSoup(contents, "lxml")
+soup = BeautifulSoup(contents, "html.parser")
+print(soup.title)
