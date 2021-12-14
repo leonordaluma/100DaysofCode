07db26bfc85e3e6ce45b7038c83365aceadbdf1f
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-14 17:03:15 +0800

    get the title challenge

--- web development/web scraping/main.py
+++ web development/web scraping/main.py
@@ -1,6 +1,13 @@
+from os import name
 from bs4 import BeautifulSoup
 import requests
 
 res = requests.get("https://news.ycombinator.com/")
 contents = res.text
 
+soup = BeautifulSoup(contents, "html.parser")
+print(soup.find(name="a", class_="titlelink").getText())
+# a_elements = soup.find_all(name="a", class_="titlelink")
+
+# for a in a_elements:
+#     print(a)
