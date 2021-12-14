e3f3680cc811c4e6432978c8ae243433264108a2
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-14 17:09:23 +0800

    get 3 pieces of data challenge

--- web development/web scraping/main.py
+++ web development/web scraping/main.py
@@ -6,7 +6,12 @@ res = requests.get("https://news.ycombinator.com/")
 contents = res.text
 
 soup = BeautifulSoup(contents, "html.parser")
-print(soup.find(name="a", class_="titlelink").getText())
+article_tag = soup.find(name="a", class_="titlelink")
+article_text = article_tag.getText()
+article_link = article_tag.get("href")
+article_score = soup.find(name="span", class_="score").getText()
+print(article_link)
+print(article_score)
 # a_elements = soup.find_all(name="a", class_="titlelink")
 
 # for a in a_elements:
