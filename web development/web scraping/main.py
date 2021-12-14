8769ec60ef8fcaee393be0d7c2485520aea83b46
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-14 16:38:19 +0800

    find particular element

--- web development/web scraping/main.py
+++ web development/web scraping/main.py
@@ -9,9 +9,12 @@ soup = BeautifulSoup(contents, "html.parser")
 
 all_anchor_tags = soup.find_all(name="a")
 
-for tag in all_anchor_tags:
-    print(tag)
-    print(tag.getText())
-    print(tag.get("href"))
+# for tag in all_anchor_tags:
+#     print(tag)
+#     print(tag.getText())
+#     print(tag.get("href"))
+
+print(soup.find(name="h1", id="name"))
+print(soup.find(name="h3", class_="heading" ))
 
 
