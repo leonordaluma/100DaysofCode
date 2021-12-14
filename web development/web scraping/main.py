fda98579504de0b0058cc431c3ffcced99b1b706
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-14 16:34:39 +0800

    get the attribute

--- web development/web scraping/main.py
+++ web development/web scraping/main.py
@@ -10,6 +10,8 @@ soup = BeautifulSoup(contents, "html.parser")
 all_anchor_tags = soup.find_all(name="a")
 
 for tag in all_anchor_tags:
+    print(tag)
     print(tag.getText())
+    print(tag.get("href"))
 
 
