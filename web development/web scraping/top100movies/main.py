e8d4b2d01cb5846657d4f27e31020b80f560d778
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-15 09:22:16 +0800

    get top 100 movies

---
+++ web development/web scraping/top100movies/main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+res = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+contents = res.text
+soup = BeautifulSoup(contents, "html.parser")
+
+data = json.loads(soup.select_one("#__NEXT_DATA__").contents[0])
+# print(json.dumps(titles, indent=4))
+
+def find_articles(data):
+    if isinstance(data, dict):
+        for k, v in data.items():
+            if k.startswith("ImageMeta:"):
+                yield v["titleText"]
+            else:
+                yield from find_articles(v)
+    elif isinstance(data, list):
+        for i in data:
+            yield from find_articles(i)
+
+
+for a in find_articles(data):
+    print(a)
