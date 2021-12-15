f36c55892f0d48de442b0850d4bb75e7de6d65a5
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-15 10:16:37 +0800

    step 1.3 scrape top 100 song titles

--- web development/web scraping/spotify-playlist/main.py
+++ web development/web scraping/spotify-playlist/main.py
@@ -1,1 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
 date_input = input("Which year do you want to travel to? (YYYY-MM-DD): ")
+url = "https://www.billboard.com/charts/hot-100/"
+res = requests.get(f"{url}/{date_input}")
+data = res.text
+
+soup = BeautifulSoup(data, "html.parser")
+top_100_songs = [song.getText() for song in soup.select(selector="li ul li h3", limit=100)]
+print(len(top_100_songs))
+print(top_100_songs)
