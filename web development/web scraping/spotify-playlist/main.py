3c4a93c2530b583e9d98a74df5bd5da542d20011
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-15 20:38:03 +0800

    step 2.4 spotify autheticaton

--- web development/web scraping/spotify-playlist/main.py
+++ web development/web scraping/spotify-playlist/main.py
@@ -1,11 +1,25 @@
 import requests
+import spotipy
 from bs4 import BeautifulSoup
+from spotipy.oauth2 import SpotifyOAuth
 from keys import client_id, client_secret
 
 
 ID = client_id
 SECRET = client_secret
 
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=ID,
+        client_secret=SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
 date_input = input("Which year do you want to travel to? (YYYY-MM-DD): ")
 url = "https://www.billboard.com/charts/hot-100/"
 res = requests.get(f"{url}/{date_input}")
@@ -13,5 +27,3 @@ data = res.text
 
 soup = BeautifulSoup(data, "html.parser")
 top_100_songs = [song.getText() for song in soup.select(selector="li ul li h3", limit=100)]
-print(len(top_100_songs))
-print(top_100_songs)
