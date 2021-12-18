a23c309709c096a8eb054c02bbcfc4bfa39478d2
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-18 19:06:13 +0800

    oculus url

--- web development/web scraping/price-tracker/main.py
+++ web development/web scraping/price-tracker/main.py
@@ -1,1 +1,1 @@
-url = "https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463"
+url = "https://www.amazon.com/Oculus-Quest-Advanced-All-One-Virtual/dp/B099VMT8VZ/ref=sr_1_1_sspa?keywords=oculus&qid=1639825393"
