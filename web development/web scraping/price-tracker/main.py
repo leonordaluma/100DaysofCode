fa3805bd8f2d368da6a94074c773b3bc9b807821
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-18 19:15:28 +0800

    step 1 hint 2 headers + hint 3 get request of actual html

--- web development/web scraping/price-tracker/main.py
+++ web development/web scraping/price-tracker/main.py
@@ -1,1 +1,13 @@
+import lxml
+import requests
+from bs4 import BeautifulSoup
+
 url = "https://www.amazon.com/Oculus-Quest-Advanced-All-One-Virtual/dp/B099VMT8VZ/ref=sr_1_1_sspa?keywords=oculus&qid=1639825393"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36 Edg/96.0.1054.53"
+accept_lang = "en-US,en;q=0.9"
+
+res = requests.get(url=url, headers={
+    "User-Agent": user_agent,
+    "Accept-Language": accept_lang
+})
+print(res.text)
