fc0c33a6263429c61999e3df92ed07a729dd183e
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-19 12:46:45 +0800

    find and select elements

--- web scraping/game-playing-bot/main.py
+++ web scraping/game-playing-bot/main.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 
 brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 chromedriver_path = "../chromedriver.exe"
-options = webdriver.ChromeOptions()
-options.binary_location = brave_path
+option = webdriver.ChromeOptions()
+option.binary_location = brave_path
+# option.add_argument("--incognito") OPTIONAL
+# option.add_argument("--headless") OPTIONAL
 
-driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+# Create new Instance of Chrome
+driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=option)
 
-driver.get("https://www.amazon.com")
-print(driver.title)
+driver.get("https://www.amazon.com/Oculus-Quest-Advanced-All-One-Virtual/dp/B099VMT8VZ/ref=sr_1_1_sspa?keywords=oculus&qid=1639825393")
+print(driver.find_element_by("priceblock_ourprice"))
+# print(driver.title)
+driver.quit()
