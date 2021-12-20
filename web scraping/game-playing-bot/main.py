972e14113e0347047067474c8bb39573097dafdc
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-20 12:27:16 +0800

    fix executable_path deprecated warning

--- web scraping/game-playing-bot/main.py
+++ web scraping/game-playing-bot/main.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome import service
+from selenium.webdriver.chrome.service import Service
 
-brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 chromedriver_path = "../chromedriver.exe"
-option = webdriver.ChromeOptions()
-option.binary_location = brave_path
-# option.add_argument("--incognito") OPTIONAL
-# option.add_argument("--headless") OPTIONAL
-
-# Create new Instance of Chrome
-driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=option)
+s = Service("../chromedriver.exe")
+driver = webdriver.Chrome(service=s)
 
 driver.get("https://www.amazon.com/Oculus-Quest-Advanced-All-One-Virtual/dp/B099VMT8VZ/ref=sr_1_1_sspa?keywords=oculus&qid=1639825393")
 print(driver.find_element_by("priceblock_ourprice"))
