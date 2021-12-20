0891f31e976a0ee4ba26520202500f0a8bc6e1a1
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-20 18:40:56 +0800

    find elements by..

--- web scraping/game-playing-bot/main.py
+++ web scraping/game-playing-bot/main.py
@@ -6,7 +6,21 @@ chromedriver_path = "../chromedriver.exe"
 s = Service("../chromedriver.exe")
 driver = webdriver.Chrome(service=s)
 
-driver.get("https://www.amazon.com/Oculus-Quest-Advanced-All-One-Virtual/dp/B099VMT8VZ/ref=sr_1_1_sspa?keywords=oculus&qid=1639825393")
-price = driver.find_element_by("priceblock_ourprice")
-print(price.text)
+driver.get("https://www.python.org/")
+# price = driver.find_element_by("priceblock_ourprice")
+# print(price.text)
+
+# search_bar = driver.find_element_by_name("q")
+# print(search_bar) #selenium element
+# print(search_bar.tag_name)
+# print(search_bar.get_attribute("placeholder"))
+
+# logo = driver.find_elements_by_class_name("python-logo")
+# print(logo.size)
+
+# docs_link = driver.find_element_by_css_selector(".documentation-widget a")
+# print(docs_link.text)
+
+
+
 driver.quit()
