dc5783a3eb0821401ce728ab4a1fd6ca8d851024
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-22 10:27:31 +0800

    type and send keys

--- web scraping/game-playing-bot/interaction.py
+++ web scraping/game-playing-bot/interaction.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chromedriver_path = "../chromedriver.exe"
 s = Service("../chromedriver.exe")
@@ -13,7 +14,11 @@ article_count = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
 # article_count.click()
 
 article_link = driver.find_element(By.LINK_TEXT,"All portals")
-article_link.click()
+# article_link.click()
+
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
 
 
 driver.quit()
