f731c8581ed0c2de4b5c4dc57f42761207158eed
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-22 10:18:31 +0800

    click link text

--- web scraping/game-playing-bot/interaction.py
+++ web scraping/game-playing-bot/interaction.py
@@ -10,5 +10,10 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 
 article_count = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
-article_count.click()
+# article_count.click()
+
+article_link = driver.find_element(By.LINK_TEXT,"All portals")
+article_link.click()
+
+
 driver.quit()
