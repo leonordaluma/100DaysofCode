d6a75a59f0025815ef729f0bcfa7b6e2e6175187
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-23 11:59:16 +0800

    auto click

--- web scraping/game-playing-bot/main.py
+++ web scraping/game-playing-bot/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
 chromedriver_path = "../chromedriver.exe"
 s = Service("../chromedriver.exe")
@@ -8,9 +9,25 @@ driver = webdriver.Chrome(service=s)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie_money = driver.find_element(By.ID, 'money')
-cookies = int(cookie_money.text)
-print(cookies)
-print(type(cookies))
+numberOfCookies = int(cookie_money.text)
+# buy_cursor = driver.find_element(By.ID, 'buyCursor')
+# buy_grandma = driver.find_element(By.ID, 'buyGrandma')
+# buy_factory = driver.find_element(By.ID, 'buyFactory')
+# buy_mine = driver.find_element(By.ID, 'buyMine')
+# buy_shipment = driver.find_element(By.ID, 'buyShipment')
+# buy_alchemy_lab = driver.find_element(By.ID, 'buyAlchemy lab')
+# buy_portal = driver.find_element(By.ID, 'buyPortal')
+# buy_time_machine = driver.find_element(By.ID, 'buyTime machine')
+cookie = driver.find_element(By.ID, 'cookie')
+timeout = 60*1
+start = time.time()
+
+while True:
+    cookie.click()
+    delta = time.time() - start
+    if delta>= timeout:
+        break
+    
 
 
 driver.quit()
