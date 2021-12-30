7c35bc7bc4b16c541ccbe548ce6f7877a14d2d96
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2021-12-30 13:57:45 +0800

    accounts

--- web scraping/twitter-bot/main.py
+++ web scraping/twitter-bot/main.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from account import username, password
 
 PROMISED_DOWN = 20.8
 PROMISED_UP = 7.42
-TWITTER_EMAIL = ''
-TWITTER_PASSWORD = ''
+TWITTER_EMAIL = username
+TWITTER_PASSWORD = password
 
 chromedriver_path = "../chromedriver.exe"
 s = Service("../chromedriver.exe")
