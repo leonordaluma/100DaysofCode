977e619f0ae206a978e02f8f4e4656fb1583aed5
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-01-07 22:28:15 +0800

    create class InstaFollower and create selenium driver

--- web scraping/instagram-bot/main.py
+++ web scraping/instagram-bot/main.py
@@ -1,6 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from credentials import username, password
 
 CHROME_DRIVER_PATH = "../chromedriver.exe"
 SIMILAR_ACCOUNT = ""
 USERNAME = username
 PASSWORD = password
+
+
+class InstaFollower:
+    def __init__(self):
+         self.s = Service("../chromedriver.exe")
+         self.driver = webdriver.Chrome(service=self.s)
+    
+    
