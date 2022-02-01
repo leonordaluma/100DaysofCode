516be669c33718b1133a73bd3b99b5bf9a935758
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-02-01 17:15:08 +0800

    google form

---
+++ capstone projects/data-entry-job-automation/main.py
@@ -0,0 +1,7 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+
+google_form = "https://docs.google.com/forms/d/e/1FAIpQLSeiq-Om2W1CKvcmXmTcCzBZ-OgPjWbvVIMnZAGknqWk6tjmpA/viewform?usp=sf_link"
+soup = BeautifulSoup()
