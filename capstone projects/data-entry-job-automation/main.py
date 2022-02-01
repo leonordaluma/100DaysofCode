9aaa582f952f68044a559272f324a8cf6d974af8
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-02-01 17:43:19 +0800

    get the price of the first rental listings

--- capstone projects/data-entry-job-automation/main.py
+++ capstone projects/data-entry-job-automation/main.py
@@ -1,7 +1,21 @@
+from email import header
 import requests
 import lxml
 from bs4 import BeautifulSoup
 
 
-google_form = "https://docs.google.com/forms/d/e/1FAIpQLSeiq-Om2W1CKvcmXmTcCzBZ-OgPjWbvVIMnZAGknqWk6tjmpA/viewform?usp=sf_link"
-soup = BeautifulSoup()
+googleform_url = "https://docs.google.com/forms/d/e/1FAIpQLSeiq-Om2W1CKvcmXmTcCzBZ-OgPjWbvVIMnZAGknqWk6tjmpA/viewform?usp=sf_link"
+zillow_url = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.70318068457031%2C%22east%22%3A-122.16347731542969%2C%22south%22%3A37.5390960357307%2C%22north%22%3A37.83957790655464%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
+headers = {
+    'content-type': 'text/html;charset=UTF-8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'   
+}
+r = requests.get(url=zillow_url, headers=headers)
+data = r.text
+
+soup = BeautifulSoup(data, 'lxml')
+price = soup.find(name="div", class_="list-card-price").getText()
+print(price)
