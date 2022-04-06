d1ed4a572a8f0535bdd51b730a5020e3cd941a64
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-04-06 20:27:43 +0800

    day 54

--- web development/flask/functions.py
+++ web development/flask/functions.py
@@ -1,22 +1,26 @@
-def decorator_function(function):
+import time
+# current_time = time.time()
+# print(current_time)
+
+def speed_calc_decorator(function):
     def wrapper_function():
+        start_time = time.time()
         function()
-        print("Leonor")
-    
+        end_time = time.time()
+        print(f"{function.__name__} run speed: {end_time - start_time}s")
+
     return wrapper_function
 
-@decorator_function
-def say_hello():
-    print("Hello")
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
 
-@decorator_function
-def say_bye():
-    print("Bye")
+@speed_calc_decorator        
+def slow_function():
+    for i in range(100000000):
+        i * i
 
-def say_greeting():
-    print("How are you?")
 
-# or
-
-decorated_function = decorator_function(say_greeting)
-decorated_function()
+fast_function()
+slow_function()
