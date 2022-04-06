844022e0c87b6427ae4fafd389f7d9aa9beb8835
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-04-06 19:47:10 +0800

    return a function from a function

---
+++ web development/flask/functions.py
@@ -0,0 +1,10 @@
+def outer_function():
+    print("I'm outer")
+    
+    def nested_function():
+        print("I'm inner")
+        
+    return nested_function
+
+inner_function = outer_function()
+inner_function()
