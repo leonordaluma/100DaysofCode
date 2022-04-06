198c2bd7aeac2b9ad0a98799705811216e06230f
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-04-06 20:09:13 +0800

    python decorators

--- web development/flask/functions.py
+++ web development/flask/functions.py
@@ -1,10 +1,22 @@
-def outer_function():
-    print("I'm outer")
+def decorator_function(function):
+    def wrapper_function():
+        function()
+        print("Leonor")
     
-    def nested_function():
-        print("I'm inner")
-        
-    return nested_function
+    return wrapper_function
 
-inner_function = outer_function()
-inner_function()
+@decorator_function
+def say_hello():
+    print("Hello")
+
+@decorator_function
+def say_bye():
+    print("Bye")
+
+def say_greeting():
+    print("How are you?")
+
+# or
+
+decorated_function = decorator_function(say_greeting)
+decorated_function()
