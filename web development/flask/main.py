2712a1544968b0593b23a320f9ea6c54330b3e28
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-04-08 18:57:05 +0800

    rendering html elements w/ flask

--- web development/flask/main.py
+++ web development/flask/main.py
@@ -7,7 +7,8 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return '<h1 style="text-align: center">Hello there!</h1> \
+            <p>This is a paragraph</p>'
 
 @app.route("/username/<name>/<int:age>")
 def greet(name, age):
