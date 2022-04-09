acbbd0922d3d2d1cac81e65904930fd40734b15b
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-04-09 19:43:45 +0800

    advanced decorators w/ args & kwargs

--- web development/flask/functions.py
+++ web development/flask/functions.py
@@ -1,26 +1,18 @@
-import time
-# current_time = time.time()
-# print(current_time)
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
 
-def speed_calc_decorator(function):
-    def wrapper_function():
-        start_time = time.time()
-        function()
-        end_time = time.time()
-        print(f"{function.__name__} run speed: {end_time - start_time}s")
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
 
-    return wrapper_function
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
 
-@speed_calc_decorator
-def fast_function():
-    for i in range(10000000):
-        i * i
-
-@speed_calc_decorator        
-def slow_function():
-    for i in range(100000000):
-        i * i
-
-
-fast_function()
-slow_function()
+new_user = User("Leonor")
+new_user.is_logged_in = True
+create_blog_post(new_user)
