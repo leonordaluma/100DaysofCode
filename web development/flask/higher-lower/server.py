27e6c5a2c382d2d5fff22b06aa8097c9ec979b41
leonordaluma <daluma.lt78@s.msumain.edu.ph>
2022-04-09 19:51:36 +0800

    higher-lower steps 1 & 2

---
+++ web development/flask/higher-lower/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def guess():
+    return "<h1>Guess a number between 0 and 9</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
